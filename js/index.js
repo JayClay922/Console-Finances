4c7900c993a4f51b503afa1d158656060e644266
JayClay922 <jamesclayman922@live.com>
2022-12-16 15:10:34 +0000

    adding heading and title to HTML, and adding pseudocode to JavaScript

--- js/index.js
+++ js/index.js
@@ -86,3 +86,17 @@ var finances = [
 ['Jan-2017', 138230],
 ['Feb-2017', 671099]
 ];
+
+// The total number of months included in the dataset.
+
+// The net total amount of Profit/Losses over the entire period.
+
+// The average of the changes in Profit/Losses over the entire period.
+
+// You will need to track what the total change in profits is from month to month and then find the average.
+
+// (Total/Number of months)
+
+// The greatest increase in profits (date and amount) over the entire period.
+
+// The greatest decrease in losses (date and amount) over the entire period.
